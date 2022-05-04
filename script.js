b96ae3ca2b3d53b6248eb85ab8974aa730499242
jonsals <jonathan.salcedomanriquez.934@my.csun.edu>
2022-05-03 22:46:42 -0700

    changes

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+var isActive = false;
 $(document).ready(function () {
   $("#share").click(function () {
-    alert("button was clicked");
+    $("#shareBox").toggle(display == true);
   });
+});
+
+document.addEventListener("click", function (event) {
+  if (isActive === true) {
+    var isClicked = document.getElementById("shareBox").contains(event.target);
+    if (!isClicked) {
+      $("#shareBox").addClass("hidden");
+    } else {
+      console.log("inside");
+    }
+  } else return;
+});
+
+$("#share").click(function () {
+  $("#shareBox").removeClass("hidden");
+  setTimeout(function () {
+    isActive = true;
+    console.log("is active");
+  }, 100);
 });
